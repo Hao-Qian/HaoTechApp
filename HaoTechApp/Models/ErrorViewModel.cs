930b27f22101410fb377695af3ffff89734b3f06
hao.qian <hao.qian@willmottdixon.co.uk>
2019-05-31 02:51:45 +0100

    Add project files.

---
+++ HaoTechApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HaoTechApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
